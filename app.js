13f2f5b332786302a0e9ccf51754ada5cb19ad0c
doctor <fd5@MacBook-Air-2022.local>
2024-01-15 20:27:26 +0700

    1

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 import express from "express";
+import { client } from "./utils/db.js";
 
 async function init() {
   const app = express();
   const port = 4000;
 
+  await client.connect();
   app.use(express.json());
-  app.use(express.urlencoded({ extended: false }));
+  app.use(express.urlencoded({ extended: true }));
 
   app.get("/", (req, res) => {
     return res.json("Hello Skill Checkpoint #2");
