2a5e5295efb14a7c9e21c7360228ec522ac6630e
Monster Cat <115872644+MonsterCatDad@users.noreply.github.com>
2024-01-15 10:14:32 +0000

    database

--- utils/db.js
+++ utils/db.js
@@ -1,2 +1,7 @@
 // Todo: Setup database connection here
-//ok
+import { MongoClient } from "mongodb";
+const url = "mongodb://localhost:27017";
+const dbName = "practice-mongo";
+
+export const client = new MongoClient(url);
+export const db = client.db(dbName);
